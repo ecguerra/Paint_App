b72470ac6829f323bdc14d73da14c5606c8677fa
Elizabeth Guerra <ecguerra@outlook.com>
2021-11-18 21:06:34 -0500

    line and color variables set

--- src/Canvas.js
+++ src/Canvas.js
@@ -5,6 +5,8 @@ export default function Canvas() {
     const canvasRef = useRef(null)
     const ctxRef = useRef(null)
     const [isDrawing, setIsDrawing] = useState(false)
+    const [lineColor, setLineColor] = useState('#000000')
+    const [lineWidth, setLineWidth] = useState(3)
 
     useEffect(()=> {
         const canvas = canvasRef.current
@@ -14,15 +16,33 @@ export default function Canvas() {
         canvas.style.height = `${window.innerHeight}px`
 
         const ctx = canvas.getContext('2d')
-
-        // These will all be variables the user can change
         ctx.scale(2,2)
         ctx.lineCap = 'round'
-        ctx.strokeStyle = 'black'
-        ctx.lineWidth = 5
-
         ctxRef.current = ctx
     }, [])
+
+    useEffect(()=> {
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+        ctx.strokeStyle = lineColor
+        ctxRef.current = ctx
+    }, [lineColor])
+
+    useEffect(()=> {
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+        ctx.lineWidth = lineWidth
+        ctxRef.current = ctx
+    }, [lineWidth])
+
+
+    const changeLineColor = e => {
+        setLineColor(e.target.value)
+    }
+
+    const changeLineWidth = e => {
+        setLineWidth(e.target.value)
+    }
 
     const startDrawing = ({nativeEvent}) => {
         const {offsetX, offsetY} = nativeEvent
@@ -45,11 +65,17 @@ export default function Canvas() {
     }
 
     return (
-        <canvas 
-            onMouseDown = {startDrawing} 
-            onMouseUp = {finishDrawing}
-            onMouseMove = {draw}
-            ref = {canvasRef}
-        />
+        <>  
+            <input type='color' id='colorPicker' name='colorPicker' aria-label='chooseColor' 
+                value={lineColor} onChange={changeLineColor}/>
+            <input type='range' id='widthSlider' name='widthSlider' aria-label='chooseLineWidth' 
+                min='1' max='20' value={lineWidth} onChange={changeLineWidth}/>
+            <canvas 
+                onMouseDown = {startDrawing} 
+                onMouseUp = {finishDrawing}
+                onMouseMove = {draw}
+                ref = {canvasRef}
+            />
+        </>
     )
 }
